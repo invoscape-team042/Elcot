8a0f60f2920bde813ce72e9346a3ed91599aaa68
SaravananM (Invoscape) <team042@invoscape.com>
2015-03-03 13:11:25 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elcot::Application.config.secret_token = 'fcfc3a10844b5a1caf1879948442034233b091d2e35eb94378968e3964ac2679ad3daf3a20eb0e0f4ce4fae675e4c8d420274d1e5cbb72a27f12f0b4dcc1be45'
